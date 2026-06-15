580222d193c4e071ac86cdbf943db74dbffa2cf9
bilawal <bilawalraja@gmail.com>
2026-06-15 14:31:36 -0700

    Auto-update Tesla inventory data

--- data.js
+++ data.js
@@ -24,6 +24,30 @@ const TESLA_INVENTORY = [
     "url": "https://www.tesla.com/my/order/7SAY272_731d8d138e8f58b641991d45a76af834?titleStatus=new&redirect=no#overview"
   },
   {
+    "vin": "7SAY238_57c3b8f722e2e163d654d522752f587e",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 54490,
+    "paint": "Ultra Red",
+    "wheels": "19’’ Crossflow Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 2,
+    "matches": [
+      "Ultra Red Paint",
+      "19\" Wheels"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPB12,$PR01,$SC04,$MDLY,$WY19P,$STY7S,$CPF0&bkba_opt=1",
+    "url": "https://www.tesla.com/my/order/7SAY238_57c3b8f722e2e163d654d522752f587e?titleStatus=new&redirect=no#overview"
+  },
+  {
     "vin": "7SAY238_0eda2b26d717f8bbb81f4855e3af2064",
     "trim": "Premium All-Wheel Drive",
     "seats": 7,
@@ -95,29 +119,6 @@ const TESLA_INVENTORY = [
     "url": "https://www.tesla.com/my/order/7SAY238_450466d1601e88c130a99092e9136704?titleStatus=new&redirect=no#overview"
   },
   {
-    "vin": "7SAY250_e59f810da20b39333d1874a651aa96f7",
-    "trim": "Premium All-Wheel Drive",
-    "seats": 7,
-    "price": 54490,
-    "paint": "Pearl White",
-    "wheels": "19’’ Crossflow Wheels",
-    "interior": "All Black Premium Interior",
-    "similarityScore": 1,
-    "matches": [
-      "19\" Wheels"
-    ],
-    "location": "Unavailable",
-    "status": "Unavailable in CA (cached)",
-    "driveTime": "N/A",
-    "flightTime": "N/A",
-    "coordinates": [
-      null,
-      null
-    ],
-    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPB12,$PPSW,$SC04,$MDLY,$WY19P,$STY7S,$CPF0,$TW01&bkba_opt=1",
-    "url": "https://www.tesla.com/my/order/7SAY250_e59f810da20b39333d1874a651aa96f7?titleStatus=new&redirect=no#overview"
-  },
-  {
     "vin": "7SAY238_c4179b0a2bc1903aa0da3eabe4c5856e",
     "trim": "Premium All-Wheel Drive",
     "seats": 7,
@@ -139,6 +140,29 @@ const TESLA_INVENTORY = [
     ],
     "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPW12,$PN01,$SC04,$MDLY,$WY20B,$STY7S,$CPF0&bkba_opt=1",
     "url": "https://www.tesla.com/my/order/7SAY238_c4179b0a2bc1903aa0da3eabe4c5856e?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_158b1741f0ff8481cad4ab7e2415db42",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 56490,
+    "paint": "Pearl White",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "Black and White Premium Interior",
+    "similarityScore": 1,
+    "matches": [
+      "White Interior"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPW12,$PPSW,$SC04,$MDLY,$WY20B,$STY7S,$CPF0&bkba_opt=1",
+    "url": "https://www.tesla.com/my/order/7SAY238_158b1741f0ff8481cad4ab7e2415db42?titleStatus=new&redirect=no#overview"
   },
   {
     "vin": "7SAY238_6afe1611d5edc984ab0ca9b438b5dbf5",
@@ -164,11 +188,11 @@ const TESLA_INVENTORY = [
     "url": "https://www.tesla.com/my/order/7SAY238_6afe1611d5edc984ab0ca9b438b5dbf5?titleStatus=new&redirect=no#overview"
   },
   {
-    "vin": "7SAY238_a5717c128e12505d44fbac00b2b2e590",
+    "vin": "7SAY238_1544f0bc7ba8c65e0ca109c503617b94",
     "trim": "Premium All-Wheel Drive",
     "seats": 7,
-    "price": 57490,
-    "paint": "Quicksilver",
+    "price": 56990,
+    "paint": "Diamond Black",
     "wheels": "20’’ Helix 2.0 Wheels",
     "interior": "Black and White Premium Interior",
     "similarityScore": 1,
@@ -176,15 +200,15 @@ const TESLA_INVENTORY = [
       "White Interior"
     ],
     "location": "Unavailable",
-    "status": "Unavailable in CA (cached)",
+    "status": "This vehicle is not available for your registration ZIP Code.",
     "driveTime": "N/A",
     "flightTime": "N/A",
     "coordinates": [
       null,
       null
     ],
-    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPW12,$PN00,$SC04,$MDLY,$WY20B,$STY7S,$CPF0&bkba_opt=1",
-    "url": "https://www.tesla.com/my/order/7SAY238_a5717c128e12505d44fbac00b2b2e590?titleStatus=new&redirect=no#overview"
+    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPW12,$PX02,$SC04,$MDLY,$WY20B,$STY7S,$CPF0&bkba_opt=1",
+    "url": "https://www.tesla.com/my/order/7SAY238_1544f0bc7ba8c65e0ca109c503617b94?titleStatus=new&redirect=no#overview"
   },
   {
     "vin": "7SAY250_db4e4aa113fd927f1c8d1a631a8707cf",
@@ -210,6 +234,29 @@ const TESLA_INVENTORY = [
     "url": "https://www.tesla.com/my/order/7SAY250_db4e4aa113fd927f1c8d1a631a8707cf?titleStatus=new&redirect=no#overview"
   },
   {
+    "vin": "7SAY250_7607267deebb4fac957714c9a59209bb",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 58490,
+    "paint": "Quicksilver",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "Black and White Premium Interior",
+    "similarityScore": 1,
+    "matches": [
+      "White Interior"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPW12,$PN00,$SC04,$MDLY,$WY20B,$STY7S,$CPF0,$TW01&bkba_opt=1",
+    "url": "https://www.tesla.com/my/order/7SAY250_7607267deebb4fac957714c9a59209bb?titleStatus=new&redirect=no#overview"
+  },
+  {
     "vin": "7SAY238_c4228e175f8ab2f357dedf831aa25e9f",
     "trim": "Premium All-Wheel Drive",
     "seats": 7,
@@ -229,5 +276,68 @@ const TESLA_INVENTORY = [
     ],
     "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPB12,$PPSW,$SC04,$MDLY,$WY20B,$STY7S,$CPF0&bkba_opt=1",
     "url": "https://www.tesla.com/my/order/7SAY238_c4228e175f8ab2f357dedf831aa25e9f?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_d0f905fef95f65b9d0d899a4f1df2a2b",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 56490,
+    "paint": "Quicksilver",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 0,
+    "matches": [],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPB12,$PN00,$SC04,$MDLY,$WY20B,$STY7S,$CPF0&bkba_opt=1",
+    "url": "https://www.tesla.com/my/order/7SAY238_d0f905fef95f65b9d0d899a4f1df2a2b?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY250_ee4eca3778e0fe5a3d4230f85dcd573e",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 56490,
+    "paint": "Pearl White",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 0,
+    "matches": [],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPB12,$PPSW,$SC04,$MDLY,$WY20B,$STY7S,$CPF0,$TW01&bkba_opt=1",
+    "url": "https://www.tesla.com/my/order/7SAY250_ee4eca3778e0fe5a3d4230f85dcd573e?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY250_0d237442e833397ca8160052fea78d8a",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 57490,
+    "paint": "Quicksilver",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 0,
+    "matches": [],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "imageUrl": "https://static-assets.tesla.com/v1/compositor/?model=my&view=STUD_3QTR&size=1440&options=$MTY48,$APFA,$IPB12,$PN00,$SC04,$MDLY,$WY20B,$STY7S,$CPF0,$TW01&bkba_opt=1",
+    "url": "https://www.tesla.com/my/order/7SAY250_0d237442e833397ca8160052fea78d8a?titleStatus=new&redirect=no#overview"
   }
 ];
