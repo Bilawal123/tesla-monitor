81c95f6513ac04df893601e725c1ef93c9c22007
bilawal <bilawalraja@gmail.com>
2026-06-14 22:33:01 -0700

    hello tesla

---
+++ data.js
@@ -0,0 +1,262 @@
+const TESLA_INVENTORY = [
+  {
+    "vin": "7SAY238_57c3b8f722e2e163d654d522752f587e",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 54490,
+    "paint": "Ultra Red",
+    "wheels": "19’’ Crossflow Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 2,
+    "matches": [
+      "Ultra Red Paint",
+      "19\" Wheels"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_57c3b8f722e2e163d654d522752f587e?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_ed6844bbc04db00b4190b21457e77fd2",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 54490,
+    "paint": "Pearl White",
+    "wheels": "19’’ Crossflow Wheels",
+    "interior": "Black and White Premium Interior",
+    "similarityScore": 2,
+    "matches": [
+      "19\" Wheels",
+      "White Interior"
+    ],
+    "location": "West Covina (1932 E GARVEY AVE S, West Covina, CA 91791-1910)",
+    "status": "Available for Immediate Pick Up",
+    "driveTime": "5.2 hrs (321 mi)",
+    "flightTime": "1.0 hr (SJC to ONT)",
+    "coordinates": [
+      34.0725,
+      -117.8925
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_ed6844bbc04db00b4190b21457e77fd2?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_0eda2b26d717f8bbb81f4855e3af2064",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 57490,
+    "paint": "Ultra Red",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "Black and White Premium Interior",
+    "similarityScore": 2,
+    "matches": [
+      "Ultra Red Paint",
+      "White Interior"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_0eda2b26d717f8bbb81f4855e3af2064?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY250_b3270c16c15db2e3a50e501a378ad2dc",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 58490,
+    "paint": "Ultra Red",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "Black and White Premium Interior",
+    "similarityScore": 2,
+    "matches": [
+      "Ultra Red Paint",
+      "White Interior"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY250_b3270c16c15db2e3a50e501a378ad2dc?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_450466d1601e88c130a99092e9136704",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 52490,
+    "paint": "Stealth Grey",
+    "wheels": "19’’ Crossflow Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 1,
+    "matches": [
+      "19\" Wheels"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_450466d1601e88c130a99092e9136704?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_6afe1611d5edc984ab0ca9b438b5dbf5",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 56490,
+    "paint": "Ultra Red",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 1,
+    "matches": [
+      "Ultra Red Paint"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_6afe1611d5edc984ab0ca9b438b5dbf5?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_a5717c128e12505d44fbac00b2b2e590",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 57490,
+    "paint": "Quicksilver",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "Black and White Premium Interior",
+    "similarityScore": 1,
+    "matches": [
+      "White Interior"
+    ],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_a5717c128e12505d44fbac00b2b2e590?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY250_db4e4aa113fd927f1c8d1a631a8707cf",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 57990,
+    "paint": "Diamond Black",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "Black and White Premium Interior",
+    "similarityScore": 1,
+    "matches": [
+      "White Interior"
+    ],
+    "location": "Long Beach (1800 E SPRING ST, LONG BEACH, CA 90755-1934)",
+    "status": "Available for Immediate Pick Up",
+    "driveTime": "5.5 hrs (335 mi)",
+    "flightTime": "1.0 hr (SJC to LGB)",
+    "coordinates": [
+      33.8114,
+      -118.1706
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY250_db4e4aa113fd927f1c8d1a631a8707cf?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_c4228e175f8ab2f357dedf831aa25e9f",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 55490,
+    "paint": "Pearl White",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 0,
+    "matches": [],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_c4228e175f8ab2f357dedf831aa25e9f?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_d17bf4fe8b009d675cee6a26e035180d",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 55990,
+    "paint": "Diamond Black",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 0,
+    "matches": [],
+    "location": "Unavailable",
+    "status": "This vehicle is not available for your registration ZIP Code.",
+    "driveTime": "N/A",
+    "flightTime": "N/A",
+    "coordinates": [
+      null,
+      null
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_d17bf4fe8b009d675cee6a26e035180d?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY238_d0f905fef95f65b9d0d899a4f1df2a2b",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 56490,
+    "paint": "Quicksilver",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 0,
+    "matches": [],
+    "location": "San Diego - Miramar (9250 Trade Pl, San Diego, CA 92121)",
+    "status": "Available for Immediate Pick Up",
+    "driveTime": "6.8 hrs (420 mi)",
+    "flightTime": "1.2 hrs (SJC to SAN)",
+    "coordinates": [
+      32.8805,
+      -117.1593
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY238_d0f905fef95f65b9d0d899a4f1df2a2b?titleStatus=new&redirect=no#overview"
+  },
+  {
+    "vin": "7SAY250_0d237442e833397ca8160052fea78d8a",
+    "trim": "Premium All-Wheel Drive",
+    "seats": 7,
+    "price": 57490,
+    "paint": "Quicksilver",
+    "wheels": "20’’ Helix 2.0 Wheels",
+    "interior": "All Black Premium Interior",
+    "similarityScore": 0,
+    "matches": [],
+    "location": "Colma (1500 Collins Ave, Colma, CA 94014)",
+    "status": "Available for Immediate Pick Up",
+    "driveTime": "1.2 hrs (75 mi)",
+    "flightTime": "N/A (Drive)",
+    "coordinates": [
+      37.6748,
+      -122.4632
+    ],
+    "url": "https://www.tesla.com/my/order/7SAY250_0d237442e833397ca8160052fea78d8a?titleStatus=new&redirect=no#overview"
+  }
+];
